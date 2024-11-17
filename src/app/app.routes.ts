6d13ca0dd286c3e49d8c90c98603863d0f710ed7
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-11-17 09:02:22 -0500

    subiendo about y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'about',component:AboutComponent}
+];
