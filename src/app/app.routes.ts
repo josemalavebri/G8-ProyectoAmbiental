7e6f6b098ec4f40c2edd6a80c9399fe9a5a1a187
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-11-18 21:10:53 -0500

    añadiendo componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { AboutComponent } from './componentes/about/about.component';
+import { SesionComponent } from './componentes/sesion/sesion.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { ActividadesComponent } from './componentes/actividades/actividades.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
-    {path:'about',component:AboutComponent}
+    {path:'about',component:AboutComponent},
+    {path:'sesion',component:SesionComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'actividades',component:ActividadesComponent}
 ];
---
+++ src/app/componentes/sesion/sesion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sesion',
+  standalone: true,
+  imports: [],
+  templateUrl: './sesion.component.html',
+  styleUrl: './sesion.component.css'
+})
+export class SesionComponent {
+
+}
