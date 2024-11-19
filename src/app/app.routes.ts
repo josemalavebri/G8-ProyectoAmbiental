cb42e4abf9c9498b669ebcc112e3ce9cd3e74bdc
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-11-19 06:42:30 -0500

    mejorando estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './componentes/about/about.component';
 import { SesionComponent } from './componentes/sesion/sesion.component';
 import { RegisterComponent } from './componentes/register/register.component';
 import { ActividadesComponent } from './componentes/actividades/actividades.component';
+import { ReportComponent } from './componentes/report/report.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'sesion',component:SesionComponent},
     {path:'register',component:RegisterComponent},
-    {path:'actividades',component:ActividadesComponent}
+    {path:'actividades',component:ActividadesComponent},
+    {path:'report',component:ReportComponent}
 ];
